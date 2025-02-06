f15e882b222f3a86c0b8078b6b48dd7b94d610b7
mustafoyev-202 <baxtiyormustafoyev2006@gmail.com>
2025-02-06 21:15:51 +0500

    first commit

--- server/server.py
+++ server/server.py
@@ -1,20 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import util
+import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('app.html')  # Serve the app.html page
 
 @app.route('/classify_image', methods=['GET', 'POST'])
 def classify_image():
     image_data = request.form['image_data']
-
     response = jsonify(util.classify_image(image_data))
-
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Sports Celebrity Image Classification")
     util.load_saved_artifacts()
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use environment variable for port
+    app.run(host='0.0.0.0', port=port)
