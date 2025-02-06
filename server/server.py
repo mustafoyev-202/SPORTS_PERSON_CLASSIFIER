958abe847db1bdb1a6716d4b0428c81472e5b766
mustafoyev-202 <baxtiyormustafoyev2006@gmail.com>
2025-02-06 21:16:17 +0500

    first commit

--- server/server.py
+++ server/server.py
@@ -4,19 +4,22 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('app.html')  # Serve the app.html page
 
-@app.route('/classify_image', methods=['GET', 'POST'])
+@app.route("/")
+def home():
+    return render_template("app.html")  # Serve the app.html page
+
+
+@app.route("/classify_image", methods=["GET", "POST"])
 def classify_image():
-    image_data = request.form['image_data']
+    image_data = request.form["image_data"]
     response = jsonify(util.classify_image(image_data))
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add("Access-Control-Allow-Origin", "*")
     return response
+
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Sports Celebrity Image Classification")
     util.load_saved_artifacts()
     port = int(os.environ.get("PORT", 5000))  # Use environment variable for port
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
